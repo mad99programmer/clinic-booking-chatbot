a856bff592dafe0ddf20bd0210864925c3e267d0
mitul <mitulms9@gmail.com>
2026-06-10 00:36:40 +0530

    added login endpoint

--- auth_routes.py
+++ auth_routes.py
@@ -1,10 +1,63 @@
-from fastapi import APIRouter
+from fastapi import APIRouter,Depends,HTTPException
+from pydantic import BaseModel
+from sqlalchemy.orm import Session
+from database import SessionLocal
+from models import Admin
+from security import verify_password
+
 
 router = APIRouter(
     prefix="/auth",
     tags=["auth"]
 )
 
+class LoginRequest(BaseModel):
+    username: str
+    password: str
+
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
 @router.get("/ping")
 def ping():
     return {"message": "auth working"}
+
+
+
+
+
+
+@router.post("/login")
+def login(
+    payload: LoginRequest,
+    db: Session = Depends(get_db)
+):
+
+    admin = db.query(Admin).filter(
+        Admin.username == payload.username
+    ).first()
+
+    if not admin:
+        raise HTTPException(
+            status_code=401,
+            detail="Invalid username or password"
+        )
+
+    if not verify_password(
+        payload.password,
+        admin.password_hash
+    ):
+        raise HTTPException(
+            status_code=401,
+            detail="Invalid username or password"
+        )
+
+    return {
+        "message": "Login successful"
+    }
